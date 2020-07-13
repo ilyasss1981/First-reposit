e7f03b8536dd3494f643f912c33cb08baded2e65
ILYAS <ilyasss1981@gmail.com>
2020-07-13 18:37:02 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+// let result = confirm('Are you here?');
+// alert(result);
+
+const answers = [];
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your lastname?', '');
+answers[2] = prompt('How old are you?', '');
+document.write(answers);
+console.log(typeof(answers));
